9cd9507f168d0593332a5e5c7ab09639d26db03b
bbares <bbares@smileidentity.com>
2020-11-03 03:29:27 -0800

    Revert "Added ID Api class"
    This reverts commit be3e6e33389227ed46f9b72f6f41fb59ec9de860.

--- SmileIdentityCore.php
+++ SmileIdentityCore.php
@@ -3,7 +3,7 @@
       require 'vendor/autoload.php';
       use ZipStream\Option\Archive as ArchiveOptions;
 
-      define('VERSION', '1.1.0');
+      define('VERSION', '1.0.0');
       define('DEFAULT_JOB_STATUS_TIMEOUT', 20);
       define('DEFAULT_JOB_STATUS_SLEEP', 2);
       define('SID_SERVERS', array(
@@ -42,73 +42,6 @@
           }
       }
 
-      class IdApi
-      {
-
-          public $sig_class;
-          private $partner_id;
-          private $default_callback;
-          private $sid_server;
-          private $js_timeout = DEFAULT_JOB_STATUS_TIMEOUT;
-
-
-          public function initialize($i_partner_id, $i_default_callback, $i_api_key, $i_sid_server)
-          {
-              $this->partner_id = $i_partner_id;
-              $this->default_callback = $i_default_callback;
-              $this->sig_class = new Signature;
-              $this->sig_class->initialize($i_partner_id, $i_api_key);
-              if(strlen($i_sid_server) == 1)
-                  if(intval($i_sid_server) < 2)
-                      $this->sid_server = SID_SERVERS[intval($i_sid_server)];
-                  else
-                      throw new Exception("Invalid server selected");
-              else
-                  $this->sid_server = $i_sid_server;
-          }
-
-
-          public function submit_job($partner_params, $id_info, $use_async)
-          {
-              $b = $this->sig_class->generate_sec_key();
-              $sec_key = $b[0];
-              $timestamp = $b[1];
-              $response = false;
-              $smile_job_id = '';
-
-              $data = array(
-                  'language' => 'php',
-                  'callback_url' => $this->default_callback,
-                  'partner_params' => $partner_params,
-                  'sec_key' => $sec_key,
-                  'timestamp' => $timestamp,
-                  'partner_id' => $this->partner_id
-                  );
-              $data = array_merge($data, $id_info);
-
-
-              $json_data = json_encode($data,JSON_PRETTY_PRINT);
-  
-              if($use_async)
-                  $ch = curl_init($this->sid_server.'/async_id_verification');
-              else
-                $ch = curl_init($this->sid_server.'/id_verification');
-              # Setup request to send json via POST.
-              curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-              curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-              curl_setopt($ch, CURLOPT_POST, 1);
-              curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data );
-              curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
-              curl_setopt($ch, CURLOPT_RETURNTRANSFER, true );
-              $response = curl_exec($ch);
-              curl_close($ch);
-              $result = json_decode($response);
-
-              return $result;
-
-          }
-      }
-
       class SmileIdentityCore
       {
 
@@ -137,7 +70,6 @@
                       throw new Exception("Invalid server selected");
               else
                   $this->sid_server = $i_sid_server;
-
           }
 
           public function generate_sec_key()
@@ -147,10 +79,10 @@
 
           private function call_prep_upload($sec_key, $timestamp, $partner_params, $filename, $options)
           {
-              if($options['optional_callback'] == null)
+              if($options->optional_callback == "")
                   $callback = $this->default_callback;
               else
-                  $callback = $options['optional_callback'];
+                  $callback = $options->optional_callback;
               $data = array(
                   'callback_url' => $callback,
                   'file_name' => $filename,
@@ -296,9 +228,9 @@
                       'image' => '',
                       'file_name' => ''
                   );
-                  if ($value['file_name'] != null)
+                  if ($value['image'] != null)
                   {
-                      $image_detail_p['file_name'] = basename($value['file_name']);
+                      $image_detail_p['file_name'] = basename($value['image']);
                   }
                   else
                   {
@@ -313,10 +245,10 @@
           private function configure_info_json($prep_upload_response_array, $id_info, $images, $partner_params, $sec_key, $timestamp, $options)
           {
 
-              if($options['optional_callback'] == "")
+              if($options->optional_callback == "")
                   $callback = $this->default_callback;
               else
-                  $callback = $options['optional_callback'];
+                  $callback = $options->optional_callback;
               $info = array(
                   "package_information" => array(
                         "apiVersion" => array(
@@ -382,9 +314,9 @@
                   $zip->addFile("info.json", $info_json);
                   foreach ($image_details as $value)
                   {
-                      if ($value['file_name'] != null)
+                      if ($value['image'] != null)
                       {
-                          $zip->addFileFromPath(basename($value['file_name']),'./' . $value['file_name']);
+                          $zip->addFileFromPath(basename($value['image']),'./' . $value['image']);
                       }
                   }
                   $zip->finish();
