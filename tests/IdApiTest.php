3538b785aba10d5e70fb4aabc3f24d038ddc8511
Agbaje Olalekan <olalekanagbaje@gmail.com>
2023-09-25 16:29:28 +0100

    clean up

--- tests/IdApiTest.php
+++ tests/IdApiTest.php
@@ -1,8 +1,6 @@
 <?php
 declare(strict_types=1);
 
-// require 'lib/IdApi.php';
-
 use GuzzleHttp\Client;
 use SmileIdentity\IdApi;
 use Ouzo\Utilities\Clock;
--- tests/SmileIdentityCoreTest.php
+++ tests/SmileIdentityCoreTest.php
@@ -1,8 +1,6 @@
 <?php
 declare(strict_types=1);
 
-// require 'lib/SmileIdentityCore.php';
-
 use GuzzleHttp\Client;
 use Ouzo\Utilities\Clock;
 use SmileIdentity\Config;
